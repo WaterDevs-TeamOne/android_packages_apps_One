LineageOS/android_packages_apps_LineageParts

parts: styles: add support for more dark overlays
Change-Id: I9730419ce6303a0418f84281789084089dc0ae25
Signed-off-by: Joey <joey@lineageos.org>

src/org/lineageos/lineageparts/style/StylePreferences.java
@@ -48,6 +48,7 @@ import org.lineageos.lineageparts.style.util.UIUtils;
 import java.util.Arrays;
 import java.util.List;
 
+import lineageos.preference.LineageSystemSettingListPreference;
 import lineageos.providers.LineageSettings;
 import lineageos.style.StyleInterface;
 import lineageos.style.Suggestion;
@@ -86,6 +87,10 @@ public class StylePreferences extends SettingsPreferenceFragment {
         mAccentPref = findPreference("style_accent");
         mAccentPref.setOnPreferenceClickListener(this::onAccentClick);
         setupAccentPref();
+
+        LineageSystemSettingListPreference darkPref = (LineageSystemSettingListPreference)
+                findPreference("berry_dark_overlay");
+        darkPref.setOnPreferenceChangeListener(this::onDarkChange);
 
         Preference automagic = findPreference("style_automagic");
         automagic.setOnPreferenceClickListener(p -> onAutomagicClick());
@@ -142,6 +147,13 @@ public class StylePreferences extends SettingsPreferenceFragment {
 
         mAccentPref.setSummary(accent.getName());
         mAccentPref.setIcon(UIUtils.getAccentBitmap(getResources(), size, accent.getColor()));
+    }
+
+    private boolean onDarkChange(Preference preference, Object newValue) {
+        if (!(newValue instanceof String)) {
+            return false;
+        }
+        return mInterface.setDarkOverlay((String) newValue);
     }
 
     private boolean onAutomagicClick() {

WaterDevs-TeamOne/android_packages_apps_One

src/com/teamone/oneparts/style/StylePreferences.java
@@ -51,6 +51,7 @@ import com.teamone.oneparts.style.util.UIUtils;
 import java.util.Arrays;
 import java.util.List;
 
+import oneos.preference.OneSystemSettingListPreference;
 import oneos.providers.oneSettings;
 import oneos.style.StyleInterface;
 import oneos.style.Suggestion;
@@ -90,6 +91,10 @@ public class StylePreferences extends SettingsPreferenceFragment {
         mAccentPref = findPreference("style_accent");
         mAccentPref.setOnPreferenceClickListener(this::onAccentClick);
         setupAccentPref();
+
+        LineageSystemSettingListPreference darkPref = (LineageSystemSettingListPreference)
+                findPreference("berry_dark_overlay");
+        darkPref.setOnPreferenceChangeListener(this::onDarkChange);
 
         Preference automagic = findPreference("style_automagic");
         automagic.setOnPreferenceClickListener(p -> onAutomagicClick());
@@ -149,6 +154,13 @@ public class StylePreferences extends SettingsPreferenceFragment {
 
         mAccentPref.setSummary(accent.getName());
         mAccentPref.setIcon(UIUtils.getAccentBitmap(getResources(), size, accent.getColor()));
+    }
+
+    private boolean onDarkChange(Preference preference, Object newValue) {
+        if (!(newValue instanceof String)) {
+            return false;
+        }
+        return mInterface.setDarkOverlay((String) newValue);
     }
 
     private boolean onAutomagicClick() {